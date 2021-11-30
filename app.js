103c797913474ce74020e48406148dc38821baea
nayru <nanualexandraramirez@gmail.com>
2021-11-30 10:18:45 -0500

    commit

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 const URL = "https://dog.ceo/api/breeds/image/random"
 const request = new XMLHttpRequest()
+
+
 request.onreadystatechange = ()=>{
     if(request.readyState === 4){
         if(request.status === 200){
             var resultado = request.responseText
             var resultado_en_objeto = JSON.parse(resultado)
             //console.log(resultado_en_objeto)
-            
+            const botoncuatro = document.getElementById('boton1');
+            const botonocho = document.getElementById('boton2');
+            const botondoce = document.getElementById('boton3');
+            const botontodas = document.getElementById('boton4');
             const section= document.querySelector('#section');
         
            
             const fragment = document.createDocumentFragment()
 
-
             dogs = Object.values(resultado_en_objeto)
-            create_card();
-            function create_card(){
+            let cantidad = 0;
+            botoncuatro.addEventListener('click', e=>{
+                cantidad = 4;
+                create_card(cantidad);
+            });
+            botonocho.addEventListener('click', e=>{
+                cantidad = 8;
+                create_card(cantidad);
+            });
+            botondoce.addEventListener('click', e=>{
+                cantidad = 12;
+                create_card(cantidad);
+            });
+            botontodas.addEventListener('click', e=>{
+                cantidad = 12;
+                create_card(cantidad);
+            });
+            
+            function create_card(cantidad){
                 console.log(dogs)
                 //for (const key in dogs) {
                 //    if (Object.hasOwnProperty.call(dogs, key)) {
@@ -31,19 +52,23 @@ request.onreadystatechange = ()=>{
                         
                  //   }
                 //}
-                for (let index = 0; index < dogs.length; index++) {
-                    const element = dogs[index];
-                    const card=document.createElement('div');
-                        const img_card=document.createElement('img');
-                        card.classList.add('card');
-                        img_card.setAttribute('id', 'image');
-                        img_card.setAttribute('src', dogs[0]);
-                        card.appendChild(img_card);
+                
+                
+                for (let index = 0; index < cantidad; index++) {
                     
-                        fragment.appendChild(card)
+                    
+                    const card=document.createElement('div');
+                    const img_card=document.createElement('img');
+                    card.classList.add('card');
+                    img_card.setAttribute('id', 'card-img-top');
+                    img_card.setAttribute('src', dogs[0]);
+                    card.appendChild(img_card);
+                    
+                    fragment.appendChild(card)
                 }
+                section.appendChild(fragment);
             }
-            section.appendChild(fragment);
+            
         }
     }
 };
